13bc685eb793c81f98a3673b7f3b81b28747ce57
xlebkin <dima-likov2710@yandex.ru>
2022-05-14 16:08:02 +0300

    add scroll & block about

--- index.js
+++ index.js
@@ -7,3 +7,17 @@ $(document).ready(function(){
 		autoplaySpeed:3500,
 	});
 });
+
+$('a[href^="#"').on('click', function() {
+
+    let href = $(this).attr('href');
+
+    $('html, body').animate({
+        scrollTop: $(href).offset().top
+    }, {
+        duration: 400,
+        easing: "linear"
+    });
+
+    return false;
+});
